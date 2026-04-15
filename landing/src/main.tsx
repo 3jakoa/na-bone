b2620e24e117248453be22c999ea24612fea347c
3jakoa <jaka.slekovec@icloud.com>
2026-04-15 22:13:57 +0200

    Revamp landing page

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Invite from "./pages/Invite";
-import Posts from "./pages/Posts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Home />} />
         <Route path="/invite" element={<Invite />} />
         <Route path="/invite/:token" element={<Invite />} />
-        <Route path="/posts" element={<Posts />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
