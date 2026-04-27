7be02fe8ae3d486c300b2d57a7973cb9d7383434
3jakoa <jaka.slekovec@icloud.com>
2026-04-27 16:16:10 +0200

    Add landing privacy and FAQ pages

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Invite from "./pages/Invite";
+import Questions from "./pages/Questions";
+import Privacy from "./pages/Privacy";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -12,6 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Home />} />
         <Route path="/invite" element={<Invite />} />
         <Route path="/invite/:token" element={<Invite />} />
+        <Route path="/vprasanja" element={<Questions />} />
+        <Route path="/faq" element={<Questions />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
